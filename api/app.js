6555ee479869fe57f075994ee63b6d5b3911b7b6
Javier <jarrindaulivi4@gmail.com>
2019-04-17 14:35:17 -0400

    signup fixed

--- api/app.js
+++ api/app.js
@@ -34,6 +34,11 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000']
+}));
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -61,12 +66,6 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost:3000']
-}));
 
 
 
