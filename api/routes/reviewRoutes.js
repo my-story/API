7dbc56adf29251141b8a424503c935db6c465474
Antonio Colmenares <colmena.anto@gmail.com>
2019-04-18 20:07:43 +0200

    merge master

---
+++ api/routes/reviewRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router  = express.Router();
+// const Review = require('../models/Review');
+// const reviewController = require("../controllers/reviewController");
+
+// router.post('/', reviewController.postReview);
+
+module.exports = router;
