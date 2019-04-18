731674f60a904123366a44056e2721cfca52ccd2
Antonio Colmenares <colmena.anto@gmail.com>
2019-04-18 18:20:53 +0200

    feat: created review model

---
+++ api/models/Review.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  title:{
+    type: String
+  },
+  influencer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Influencer'
+  },
+  product: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  }],
+  review: {
+    type: String
+  },
+  likes:{
+    type: Array
+  },
+  video:{
+    type: String
+  },
+  photo:{
+    type: String
+  },
+  voicenote:{
+    type: String
+  }
+}, {timestamps: true});
+
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
