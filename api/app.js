a3fd2a1757ab9a9027fb8fdee1e835f7e6f9deb2
Antonio Colmenares <colmena.anto@gmail.com>
2019-04-30 13:24:14 -0400

    signup and loggin workinggit statusgit status

--- api/app.js
+++ api/app.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
 
 const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
+// const hbs          = require('hbs');
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 const session       = require('express-session');
 const passport      = require('passport');
+const User = require('./models/User');
+const MongoStore = require('connect-mongo')(session)
 // const MongoStore = require('connect-mongo')(session)
 const cors = require('cors');
+
 // require cors as our security package to enable our API to receive requests from our React app
 
-require('./config/passport-stuff')
+// require('./config/passport-stuff')
 // require in the configuration code we put in config/passport-stuff.js
 
 
@@ -41,8 +45,7 @@ app.use(cors({
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Express View engine setup
 
@@ -59,20 +62,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
 app.use(session({
-  secret:"some secret goes here",
-  resave: true,
-  saveUninitialized: true
+  secret:process.env.SECRET,
+  store: new MongoStore({ url: 'mongodb://localhost/test-app' }),
+  resave: false, 
+  saveUninitialized: false 
 }));
 
 // app.use(session({ secret: 'anything' }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(require("body-parser").text());
 
 
 const index = require('./routes/index');
@@ -96,6 +102,38 @@ app.use('/influencer/rewards', influencerRewardRoutes);
 const reviewRoutes = require('./routes/reviewRoutes');
 app.use('/reviews', reviewRoutes);
 
+
+app.use((req, res, next) => {
+  res.locals.session = req.user;
+  next();
+})
+
+// 404
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+// app.use(function (error, req, res, next) {
+//   console.error(error);
+
+//   res.status(error.status || 500);
+
+//   const data = {}
+
+//   if (error instanceof mongoose.Error.ValidationError) {
+//     res.status(400);
+//     for (field of Object.keys(error.errors)) {
+//       error.errors[field] = error.errors[field].message
+//     }
+//     data.errors = error.errors
+//   } else if (error instanceof mongoose.Error.CastError) {
+//     error = createError(404, 'Resource not found')
+//   }
+
+//   data.message = error.message;
+//   res.json(data);
+// });
 
 
 
