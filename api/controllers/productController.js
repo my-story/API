5b3409ba9fb453025031f612f3d2145ac1b1122e
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-04-30 16:56:49 -0400

    Splapatap

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -22,15 +22,9 @@ module.exports.createProduct = (req,res,next)=>{
     "influencer": req.body.influencer
   })
   .then((p)=>{
-    // console.log(p._id)
     test = p._id
     res.status(201).json(p)
-    // Product.findByIdAndUpdate(p._id,
-    //   {picture: req.profilePic.data})
-    // .then( product => {
-    //   // console.log(product)
-    //   res.status(200).json({ message: "Product succesfully updated", picture: req.file.url })
-    // })
+
   })
   .catch((e)=>next(e))
 }
