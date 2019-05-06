ee70bb419fc8a6fc377c678023374a4c069f05d2
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-05-06 13:00:44 -0400

    Audio Routes

--- api/routes/reviewRoutes.js
+++ api/routes/reviewRoutes.js
@@ -2,7 +2,9 @@
 const express    = require('express');
 const router     = express.Router();
 const Review   = require('../models/Review');
+const uploadCloud = require('../config/cloudinary')
 
+let test = {}
 router.get('/all', (req, res, next) =>{
     console.log('in reviews',req.user, req.params)
 
@@ -30,14 +32,28 @@ router.get('/details/:id', (req, res, next) =>{
 
 
 router.post('/new', (req, res, next) =>{
+    console.log("el req" + req)
     Review.create(req.body)
         .then((response) =>{
+            test = response._id
+            console.log(response)
             res.json(response)
         })
         .catch((err) =>{
             res.json(err);
         })
 })
+
+//Add voicenote 
+router.post('/upload/voicenote',uploadCloud.single('video'),(req,res,next)=>{
+    // const id = req.params
+    Review.findByIdAndUpdate(test,{voicenote : req.file.url}, {new:true})
+    .then((review)=>{
+        console.log(review)
+      res.status(201).json(review)
+    })
+    .catch((e)=>next(e))
+    })
 
 router.post('/edit/:id', (req, res, next) =>{
     // console.log('reqs',req.params, req.body, req.user)
