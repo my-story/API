ebfd8e8c4fb1c994babe0629413a0f82e9789148
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-05-10 11:54:16 -0400

    Product listing

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -4,15 +4,16 @@ let test = {}
 
 module.exports.getAll = (req,res,next)=>{
   Product.find()
-  .populate('influencer')
+  .populate("influencer")
   .then((p)=>{
+    console.log(p)
     res.status(200).json(p)
   })
   .catch()
 }
 
 module.exports.createProduct = (req,res,next)=>{
-  console.log(req.body)
+
 
   Product.create({
     model: req.body.model,
@@ -20,7 +21,7 @@ module.exports.createProduct = (req,res,next)=>{
     // images:req.body.images,
     category: req.body.category,
     prize: req.body.prize,
-    // influencer: req.body.influencer
+    influencer: req.body.influencer
   })
   .then((response)=>{
     test = response._id
