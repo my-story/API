71b932e28f9d0f0e879188dd08bc4a465f9b8923
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-05-10 13:05:17 -0400

    Added Stripe packages

---
+++ api/routes/stripeRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router  = express.Router();
+
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+// router.post(('/stripe'),(async () => {
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ['card'],
+//     line_items: [{
+//       name: 'T-shirt',
+//       description: 'Comfortable cotton t-shirt',
+//       images: ['https://example.com/t-shirt.png'],
+//       amount: 500,
+//       currency: 'usd',
+//       quantity: 1,
+//     }],
+//     success_url: 'https://example.com/success',
+//     cancel_url: 'https://example.com/cancel',
+//   });
+// }))();
+
+module.exports = router;
