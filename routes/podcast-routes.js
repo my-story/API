0cc7eb99821d441482b6dcc56ec1d40e5b36b519
sebasgross <sebasgrossmann@gmail.com>
2020-04-02 14:22:30 -0400

    podast

--- routes/podcast-routes.js
+++ routes/podcast-routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const podcastController = require('../controllers/podcast-controller')
 
-router.get('/', podcastController.getAll);
+router.get('/all', podcastController.getAll);
 router.post('/create', podcastController.create);
-router.get('/:id', podcastController.getOne);
+router.get('/detail/:id', podcastController.getOne);
 router.post('/:id', podcastController.edit);
 
 module.exports = router;
