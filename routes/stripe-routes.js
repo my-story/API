c5e920ac34f6c19a4c46589c493151335301ad63
sebasgross <sebasgrossmann@gmail.com>
2020-05-26 14:54:49 -0400

    no more stripe

--- routes/stripe-routes.js
+++ routes/stripe-routes.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router  = express.Router();
-const stripeController = require("../controllers/stripe-controller");
+// const express = require('express');
+// const router  = express.Router();
+// const stripeController = require("../controllers/stripe-controller");
 
-router.use(require("body-parser").text());
+// router.use(require("body-parser").text());
 
-router.post("/charge", stripeController.charge);
+// router.post("/charge", stripeController.charge);
 
-module.exports = router;
+// module.exports = router;
